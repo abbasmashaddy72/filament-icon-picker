73e570c2de7f1e0e31b7f6433000309b2f0997bb
abbasmashaddy72 <abbasmashaddy72@gmail.com>
2025-06-23 18:47:22 +0530

    Filanent v4 Fix

--- src/Forms/IconPicker.php
+++ src/Forms/IconPicker.php
@@ -27,7 +27,7 @@ class IconPicker extends Select
     protected array|Closure|null $disallowedIcons = null;
 
     protected bool|Closure $isHtmlAllowed = true;
-    protected bool|Closure $isSearchable = true;
+    protected Closure|bool|null $isSearchable = true;
 
     protected Closure|string|Htmlable|null $itemTemplate = null;
 
